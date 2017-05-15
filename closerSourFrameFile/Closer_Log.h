f0ce5b4ad9246ac9116ebbb88b4fbe3d6b8188fd
jihaihui <15195774380@163.com>
2017-05-15 16:28:51 +0800

    1.1 pod
    11

---
+++ closerSourFrameFile/Closer_Log.h
@@ -0,0 +1,14 @@
+//
+//  Closer_Log.h
+//  closerframework
+//
+//  Created by jihh on 17/5/15.
+//  Copyright © 2017年 QB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Closer_Log : NSObject
+
+-(void)log_Worn;
+@end
---
+++ closerSourFrameFile/Closer_Log.m
@@ -0,0 +1,17 @@
+//
+//  Closer_Log.m
+//  closerframework
+//
+//  Created by jihh on 17/5/15.
+//  Copyright © 2017年 QB. All rights reserved.
+//
+
+#import "Closer_Log.h"
+
+@implementation Closer_Log
+
+-(void)log_Worn
+{
+    NSLog(@"输出各种警告");
+}
+@end
